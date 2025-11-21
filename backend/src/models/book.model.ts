3a0d8ef0649a836e6c7802c4c05dafe1067db75f
PrithiviPM2580 <prithivipemimagar>
2025-11-21 17:59:03 +0545

    feat: Add Book model with schema definition and transformation methods

---
+++ backend/src/models/book.model.ts
@@ -0,0 +1,78 @@
+// ============================================================
+// 🧩 BookModel — Book data model and schema definition
+// ============================================================
+import mongoose, { Schema, HydratedDocument, Model, Types } from "mongoose";
+
+// ------------------------------------------------------
+// Define Book Interface
+// ------------------------------------------------------
+export interface IChapter {
+  title: string;
+  description: string;
+  content: string;
+}
+
+export interface IBook {
+  __id: Types.ObjectId;
+  userId: Types.ObjectId;
+  title: string;
+  subtitle: string;
+  author: string;
+  coverImageUrl: string;
+  chapters: IChapter[];
+  status: "draft" | "published";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Document and Mode type
+export type BookDocument = HydratedDocument<IBook>;
+export type BookModelType = Model<IBook>;
+export type BookObject = IBook;
+
+// ------------------------------------------------------
+// Define Book Schema
+// ------------------------------------------------------
+const chapterSchema = new Schema<IChapter>({
+  title: { type: String, required: true, trim: true },
+  description: { type: String, required: true, trim: true, default: "" },
+  content: { type: String, required: true, trim: true, default: "" },
+});
+
+const bookSchema = new Schema<IBook, BookModelType>(
+  {
+    __id: { type: Schema.Types.ObjectId, required: true, auto: true },
+    userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    title: { type: String, required: true, trim: true },
+    subtitle: { type: String, trim: true, default: "" },
+    author: { type: String, required: true, trim: true },
+    coverImageUrl: { type: String, default: null },
+    chapters: { type: [chapterSchema], default: [] },
+    status: { type: String, enum: ["draft", "published"], default: "draft" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// ------------------------------------------------------
+// Transform output (remove password, __v)
+// ------------------------------------------------------
+bookSchema.set("toJSON", {
+  // it defines a custom transformation function that modifies the output when a document is converted to JSON.
+  transform(_, ret: Partial<IBook> & { __v?: number }) {
+    delete ret.__v;
+    return ret;
+  },
+});
+
+// ------------------------------------------------------
+// Virtuals (it will not be stored in DB and sued to query and populate data)
+// ------------------------------------------------------
+
+// ------------------------------------------------------
+// Booke Model export
+// ------------------------------------------------------
+const BookModel = mongoose.model<IBook, BookModelType>("Book", bookSchema);
+
+export default BookModel;
