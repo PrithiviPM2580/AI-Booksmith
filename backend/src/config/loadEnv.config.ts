73424264332fc425d3ad966b1c88dd2901247279
PrithiviPM2580 <prithivipemimagar>
2025-11-21 13:44:35 +0545

    Add database and environment configuration files with connection handling

---
+++ backend/src/config/loadEnv.config.ts
@@ -0,0 +1,14 @@
+// ============================================================
+// 🧩 LoadEnvConfig — Loads environment variables configuration
+// ============================================================
+
+import path from "node:path";
+import dotenv from "dotenv";
+
+const ENV = process.env.NODE_ENV || "development";
+
+const envFile = `.env.${ENV}`;
+
+dotenv.config({
+  path: path.resolve(process.cwd(), envFile),
+});
