b9a96a0acfdd23d638da5fd2c3bb7d50514eaf84
PrithiviPM2580 <prithivipemimagar>
2025-11-22 14:15:48 +0545

    feat: add Cloudinary configuration and upload utility, update environment variables for cloud storage

---
+++ backend/src/config/cloudinary.config.ts
@@ -0,0 +1,17 @@
+// ============================================================
+// 🧩 CloudinaryConfig — Configuration for Cloudinary cloud storage
+// ============================================================
+
+import { v2 as cloudinary } from "cloudinary";
+import config from "@/config/env.config.js";
+
+// Cloudinary configuration
+cloudinary.config({
+  cloud_name: config.CLOUD_STORAGE_CLOUD_NAME,
+  api_key: config.CLOUD_STORAGE_API_KEY,
+  api_secret: config.CLOUD_STORAGE_API_SECRET,
+  secure: true,
+  upload_prefix: config.CLOUD_STORAGE_UPLOAD_PREFIX,
+});
+
+export default cloudinary;
